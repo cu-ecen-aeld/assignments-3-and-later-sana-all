ea5d8f5e7262fc8f2a4bf9d8203806c92ecdd07d
sana-all <magigingbestlawyerako21@gmail.com>
2025-09-24 15:07:26 +0800

    test 12

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -55,7 +55,8 @@ fi
 
 for i in $( seq 1 $NUMFILES)
 do
-	./writer.sh "$WRITEDIR/${username}$i.txt" "$WRITESTR"
+# ./writer.sh "$WRITEDIR/${username}$i.txt" "$WRITESTR" # past
+	writer.sh "$WRITEDIR/${username}$i.txt" "$WRITESTR"
 done
 
 OUTPUTSTRING=$(/usr/bin/finder.sh "$WRITEDIR" "$WRITESTR")
