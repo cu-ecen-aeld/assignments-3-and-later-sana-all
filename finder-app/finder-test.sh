7f1ccf1353560f0cc1e63267fac5053572af0291
sana-all <magigingbestlawyerako21@gmail.com>
2025-09-25 13:49:58 +0800

    test 21

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -9,7 +9,8 @@ NUMFILES=10
 WRITESTR=AELD_IS_FUN
 WRITEDIR=/tmp/aeld-data
 # username=$(cat conf/username.txt)
-username=$(cat /etc/finder-app/conf/username.txt)
+# username=$(cat /etc/finder-app/conf/username.txt)
+username=$(cat ./username.txt)
 
 if [ $# -lt 3 ]
 then
@@ -34,8 +35,9 @@ rm -rf "${WRITEDIR}"
 
 # create $WRITEDIR if not assignment1
 # assignment=`cat ../conf/assignment.txt`
-assignment=`cat /etc/finder-app/conf/assignment.txt`
+# assignment=`cat /etc/finder-app/conf/assignment.txt`
 # assignment=`cat conf/assignment.txt`
+assignment=`cat ./assignment.txt`
 
 if [ $assignment != 'assignment1' ]
 then
