cf1f5a514d9e42954a937d645f0ac323f5b4f90e
sana-all <magigingbestlawyerako21@gmail.com>
2025-09-25 13:34:22 +0800

    test 20 a bit change did not use home trick

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -8,7 +8,8 @@ set -u
 NUMFILES=10
 WRITESTR=AELD_IS_FUN
 WRITEDIR=/tmp/aeld-data
-username=$(cat conf/username.txt)
+# username=$(cat conf/username.txt)
+username=$(cat /etc/finder-app/conf/username.txt)
 
 if [ $# -lt 3 ]
 then
@@ -33,8 +34,8 @@ rm -rf "${WRITEDIR}"
 
 # create $WRITEDIR if not assignment1
 # assignment=`cat ../conf/assignment.txt`
-# assignment=`cat /etc/finder-app/conf/assignment.txt`
-assignment=`cat conf/assignment.txt`
+assignment=`cat /etc/finder-app/conf/assignment.txt`
+# assignment=`cat conf/assignment.txt`
 
 if [ $assignment != 'assignment1' ]
 then
@@ -56,8 +57,8 @@ fi
 
 for i in $( seq 1 $NUMFILES)
 do
-  ./writer.sh "$WRITEDIR/${username}$i.txt" "$WRITESTR" 
-	# writer.sh "$WRITEDIR/${username}$i.txt" "$WRITESTR"
+  #./writer.sh "$WRITEDIR/${username}$i.txt" "$WRITESTR" 
+	writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
 done
 
 #OUTPUTSTRING=$(/usr/bin/finder.sh "$WRITEDIR" "$WRITESTR")
