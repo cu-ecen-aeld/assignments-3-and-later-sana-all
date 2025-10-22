6ca4bbf7dd605f88888e2fbb3dd5b29301f6eb8c
sana-all <magigingbestlawyerako21@gmail.com>
2025-10-22 20:13:05 +0800

    Finishing up aesdsocket part 2

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -11,11 +11,15 @@
 #include <syslog.h>
 #include <arpa/inet.h>
 #include <errno.h>
+#include <stdbool.h>
 
 
 #define PORT "9000"
 #define BUFFER_SIZE 1024
 #define DATA_FILE_PATH "/var/tmp/aesdsocketdata"
+
+
+bool sig_quit = false;
 
 
 void error(const char *msg)
@@ -24,24 +28,61 @@ void error(const char *msg)
 	exit(1);
 }
 
+void signal_handler(int n)
+{
+	syslog(LOG_INFO, "Caught signal, exiting");
+	sig_quit = true;
+}
+
+void send_data_to_client(int sockfd)
+{
+	int fd = open("/var/tmp/aesdsocketdata", O_RDWR|O_CREAT|O_APPEND, 0600);
+	if(fd < 0)
+	{
+		error("send_data_to_client, open function error...");
+	}
+	char buffer[BUFFER_SIZE];
+	bzero(buffer, BUFFER_SIZE);
+	// n = read(sockfd, buffer, BUFFER_SIZE);
+	// if(n < 0)
+	// 	error("Error on read");
+
+	ssize_t bytes_received;
+	bytes_received = recv(sockfd, buffer, BUFFER_SIZE - 1, 0);
+	if(bytes_received < 0)
+	{
+		error("error send data to client...");
+	}
+	buffer[bytes_received] = '\0';
+	if( (write(fd, buffer, bytes_received)) < 0 )
+	{
+		error("send_data_to_client, write function error...");
+	}
+
+	if( (close(fd)) < 0)
+	{
+		error("send_data_to_client, close function error...");
+	}
+}
+
 
 int main(int argc, char *argv[])
 {
-	int sockfd, newsockfd, portno, n;
+	int sockfd, newsockfd, portno;
 	char buffer[BUFFER_SIZE];
 
-	struct sockaddr_in serv_addr, cli_addr;
+	struct sockaddr_in serv_addr;
 	// socklen_t clilen;
 
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	int portno = atoi(PORT);
+	portno = atoi(PORT);
 
 
 	serv_addr.sin_family = AF_INET;
 	serv_addr.sin_addr.s_addr = INADDR_ANY;
 	serv_addr.sin_port = htons(portno);
 
-	if( bind(sockfd), (struct sockaddr*) &serv_addr, sizeof(serv_addr) < 0)
+	if( bind(sockfd, (struct sockaddr*) &serv_addr, sizeof(serv_addr)) < 0)
 	{
 		error("binding failed...");
 	}
@@ -56,23 +97,52 @@ int main(int argc, char *argv[])
 	}
 
 
-	newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, sizeof(cli_addr));
-	if(newsockfd < 0)
-	{
-		error("accepting failed...");
-	}
+	struct sigaction new_action;
+    memset(&new_action, 0, sizeof(new_action) );
+    new_action.sa_handler = signal_handler;
+
+    if( sigaction(SIGINT, &new_action, NULL) < 0 || sigaction(SIGTERM, &new_action, NULL) < 0 )
+    {
+    	error("sigaction failed...");
+    }
 
 
-	char client_ip[INET_ADDRSTRLEN]; // for ipv6 client_ip[INET6_ADDRSTRLEN];
-	// if( getnameinfo((struct sockaddr *) &cli_addr, sizeof(cli_addr) ) ) // i commented this because getnameinfo is very very long
-	if( inet_ntop(AF_INET, &cli_addr.sin_addr, client_ip, sizeof(client_ip)) == NULL )
-	{
-		error("inet_ntop failed...");
-	}
-	syslog(LOG_INFO,  "Accepted connection from %s", client_ip);
+    while( sig_quit == false )
+    {
+    	struct sockaddr_in cli_addr;
+    	socklen_t clilen;
+    	clilen = sizeof(cli_addr);
+    	newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+		if(newsockfd < 0)
+		{
+			error("accepting failed...");
+		}
 
 
-	int fd = open("/var/tmp/aesdsocketdata", O_RDWR|O_CREAT|O_APPEND, 0600);
+		char client_ip[INET_ADDRSTRLEN]; // for ipv6 client_ip[INET6_ADDRSTRLEN];
+		// if( getnameinfo((struct sockaddr *) &cli_addr, sizeof(cli_addr) ) ) // i commented this because getnameinfo is very very long
+		if( inet_ntop(AF_INET, &cli_addr.sin_addr, client_ip, sizeof(client_ip)) == NULL )
+		{
+			error("inet_ntop failed...");
+		}
+		syslog(LOG_INFO,  "Accepted connection from %s", client_ip);
+
+
+		send_data_to_client(newsockfd);
+
+
+    }
+
+
+
+
+
+
+
+	// int fd = open("/var/tmp/aesdsocketdata", O_RDWR|O_CREAT|O_APPEND, 0600);
+
+
+
 
 
 
