6a09fa569578b9d1bdec075e0687b197bd45799a
sana-all <magigingbestlawyerako21@gmail.com>
2025-12-03 18:37:53 +0800

    commit 35

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -54,7 +54,7 @@ void *timestamp_thread(void *arg){
 
 
 	    
-	    sleep(1);
+	    sleep(9);
 	    t = time(NULL);
 	    t_ptr = localtime(&t);
 	    strftime(buffer,BUFFER_SIZE, "timestamp:%F %T\n", t_ptr);
