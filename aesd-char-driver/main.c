00def4f49ae473ab84345c1c2eb13a25786b8731
sana-all <magigingbestlawyerako21@gmail.com>
2026-06-04 10:43:20 +0800

    test 4

--- aesd-char-driver/main.c
+++ aesd-char-driver/main.c
@@ -11,10 +11,10 @@
  *
  */
 
-#include <linux/module.h>
+#include <linux/module.h> // included in deadcode
 #include <linux/init.h>
 #include <linux/printk.h>
-#include <linux/types.h>
+#include <linux/types.h> // #include <stdbool.h> for dead code
 #include <linux/cdev.h>
 #include <linux/fs.h> // file_operations // fs.inode
 #include <linux/mutex.h>
@@ -24,10 +24,25 @@
 #include "aesd-circular-buffer.h"
 #include <linux/device.h>
 #include "aesd_ioctl.h"
+
+
+// start-----------------------------------------------------------------
+// dead codes, for testing only
+#ifdef __KERNEL__
+#include <linux/string.h>
+#else
+#include <string.h>
+#endif
+#include <fcntl.h>
+#include <stdint.h>
+#include <linux/kernel.h>
+//end--------------------------------------------------------------------
+
+
 int aesd_major =   0; // use dynamic majorr
 int aesd_minor =   0;
 
-// -----------------------------------------------------------------
+// start-----------------------------------------------------------------
 // dead codes, for testing only
 typedef char unsigned u8;
 typedef short unsigned u16;
@@ -44,10 +59,235 @@ typedef long long s64;
 #define min(a,b) (((a) < (b)) ? (a) : (b))
 #define max(a,b) (((a) > (b)) ? (a) : (b))
 #define OUT_SZ 256
-//--------------------------------------------------------------------
+// end--------------------------------------------------------------------
+
+
+// start-----------------------------------------------------------------
+// dead codes, for testing only
+
+
+enum MnemonicId {
+    MN_UNDEF = 0,
+    MN_MOV,
+    MN_ADD,
+    MN_SUB,
+    MN_CMP,
+    MN_JNE,
+    MN_COUNT
+};
+
+enum data_transfer_type_num {
+    rm_tf_fr = 0,
+    i_t_rm = 1,
+    i_t_r = 2,
+    m_t_a = 3,
+    a_t_m = 4
+};
+
+static const char *mnemonic_names[MN_COUNT] = {
+     "UNDEF", "mov", "add", "sub", "cmp", "jne"
+};
+static u8 mnemonic_index[256];
+
+void init_table(void){
+    int i;
+    for (i = 0; i < 256; ++i){
+        mnemonic_index[i] = MN_UNDEF;
+    }
+    mnemonic_index[0b10001000] = 1;
+    mnemonic_index[0b10110000] = 1;
+    mnemonic_index[0b11000110] = 1;
+    mnemonic_index[0b10100000] = 1;
+    mnemonic_index[0b10100010] = 1;
+    mnemonic_index[0] = 2;
+    mnemonic_index[0b10000000] = 2;
+    mnemonic_index[0b00000100] = 2;
+    mnemonic_index[0b00101000] = 3;
+    mnemonic_index[0b00101100] = 3;
+    mnemonic_index[0b00101100] = 3;
+    mnemonic_index[0b00111000] = 4;
+    mnemonic_index[0b00111100] = 4;
+    mnemonic_index[0b01110101] = 5;
+    //mnemonic_index[0b00111100] = 4;
+}
+
+
+// end--------------------------------------------------------------------
+
 
 MODULE_AUTHOR("Mcrey Fonacier"); /** TODO: fill in your name **/
 MODULE_LICENSE("Dual BSD/GPL");
+
+// start-----------------------------------------------------------------
+// dead codes, for testing only
+
+enum MnemonicId {
+    MN_UNDEF = 0,
+    MN_MOV,
+    MN_ADD,
+    MN_SUB,
+    MN_CMP,
+    MN_JNE,
+    MN_COUNT
+};
+
+enum data_transfer_type_num {
+    rm_tf_fr = 0,
+    i_t_rm = 1,
+    i_t_r = 2,
+    m_t_a = 3,
+    a_t_m = 4
+};
+
+static const char *mnemonic_names[MN_COUNT] = {
+     "UNDEF", "mov", "add", "sub", "cmp", "jne"
+};
+static u8 mnemonic_index[256];
+
+void init_table(void){
+    int i;
+    for (i = 0; i < 256; ++i){
+        mnemonic_index[i] = MN_UNDEF;
+    }
+    mnemonic_index[0b10001000] = 1;
+    mnemonic_index[0b10110000] = 1;
+    mnemonic_index[0b11000110] = 1;
+    mnemonic_index[0b10100000] = 1;
+    mnemonic_index[0b10100010] = 1;
+    mnemonic_index[0] = 2;
+    mnemonic_index[0b10000000] = 2;
+    mnemonic_index[0b00000100] = 2;
+    mnemonic_index[0b00101000] = 3;
+    mnemonic_index[0b00101100] = 3;
+    mnemonic_index[0b00101100] = 3;
+    mnemonic_index[0b00111000] = 4;
+    mnemonic_index[0b00111100] = 4;
+    mnemonic_index[0b01110101] = 5;
+    //mnemonic_index[0b00111100] = 4;
+}
+
+typedef struct{
+    u8 key;
+    const char *value;
+} umap;
+
+typedef struct{
+    u8 key;
+    const char *value;
+} umap_r;
+
+typedef struct{
+    char *mnemonic;
+    u8 data_transfer_type;
+    bool destination;
+    bool wide;
+    bool displacement;
+    bool data_avail;
+    bool has_add;
+} lmi_form; // left_machine_instruction_form
+
+typedef struct{
+    u16 data;
+    u16 disp_data;
+    char *s_reg;
+    char *s_rm;
+    bool bracket;
+    bool sw;
+} lmi_form_right; // left_machine_instruction_form
+
+
+umap nem[] = {
+    {0b100010, "mov"}, // Register/memory to/from register
+    {0b1100011, "mov"},// immediate to register/memory
+    {0b1011, "mov"}, // immediate to register
+    {0b1010000, "mov"}, // memory to accumulator
+    {0b1010001, "mov"}, // accumulator to memory
+    {0b10001110, "mov"}, // Register/memory to segment register
+    {0b10001100, "mov"},// Segmenet register to register/memory
+    // ----------------------------------------------------------------//
+    {0b0, "add"},
+    {0b100000, "add"},
+    {0b0000010, "add"},
+    // ----------------------------------------------------------------//
+    {0b001010, "sub"},
+    // {0b100000, "sub"},
+    {0b0010110, "sub"},
+    // ----------------------------------------------------------------//
+    {0b001110, "cmp"},
+    {0b0011110, "cmp"},
+    // ----------------------------------------------------------------//
+    {0b01110101, "jne"}
+};
+
+//umap reg_w[] = {
+//    {0b000, "ax"},
+//    {0b001, "cx"},
+//    {0b010, "dx"},
+//    {0b011, "bx"},
+//    // _________________ space for readability
+//    {0b100, "sp"},
+//    {0b101, "bp"},
+//    {0b110, "si"},
+//    {0b111, "di"},
+//};
+
+static const char *reg_w[9] = {
+    "ax", "cx", "dx", "bx", "sp", "bp", "si", "di", ""
+};
+
+static const char *reg_nw[9] = {
+    "al", "cl", "dl", "bl", "ah", "ch", "dh", "bh", ""
+};
+
+
+static const char *rm_00[9] = {
+    "bx + si", "bx + di", "bp + si", "bp + di", "si", "di", "bp", "bx", ""
+};
+
+//static const char *rm_01[8] = {
+//    "al", "cl", "dl", "bl", "ah", "ch", "dh", "bh"
+//};
+//
+//static const char *rm_10[8] = {
+//    "al", "cl", "dl", "bl", "ah", "ch", "dh", "bh"
+//};
+
+//umap reg_nw[] = {
+//    {0b000, "al"},
+//    {0b001, "cl"},
+//    {0b010, "dl"},
+//    {0b011, "bl"},
+//    // _________________ space for readability
+//    {0b100, "ah"},
+//    {0b101, "ch"},
+//    {0b110, "dh"},
+//    {0b111, "bh"},
+//};
+
+//umap rm[] = {
+//    {0b000, "bx + si"},
+//    {0b001, "bx + di"},
+//    {0b010, "bp + si"},
+//    {0b011, "bp + di"},
+//    // _________________ space for readability
+//    {0b100, "si"},
+//    {0b101, "di"},
+//    {0b110, "bp"}, // if mod == 00 it is direct address
+//    {0b111, "bx"},
+//};
+
+static const char *rm[8] = {
+    "bx + si", /* 0 */
+    "bx + di", /* 1 */
+    "bp + si", /* 2 */
+    "bp + di", /* 3 */
+    "si",      /* 4 */
+    "di",      /* 5 */
+    "bp",      /* 6 -> special when mod == 0 */
+    "bx"       /* 7 */
+};
+
+// end--------------------------------------------------------------------
 
 struct aesd_dev aesd_device;
 
@@ -84,7 +324,7 @@ int aesd_release(struct inode *inode, struct file *filp)
 ssize_t aesd_read(struct file *filp, char __user *buf, size_t count,
                 loff_t *f_pos)
 {
-    int rr;
+    int rc;
     struct aesd_dev *device;
     size_t offset;
     struct aesd_buffer_entry *entry;
